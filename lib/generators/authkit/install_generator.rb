d59ca9a0a199f25c7313001b1b934dd74b20716d
Jeff Rafter <jeffrafter@gmail.com>
2013-10-18 13:57:33 -0700

    Adding the migration templates

--- lib/generators/authkit/install_generator.rb
+++ lib/generators/authkit/install_generator.rb
@@ -9,6 +9,18 @@ module Authkit
     end
 
     def generate_authkit
+      generate_migration("create_users")
+      generate_migration("add_authkit_fields_to_users")
+    end
+
+    protected
+
+    def generate_migration(filename)
+      if self.class.migration_exists?("db/migrate", "#{filename}")
+        say_status("skipped", "Migration #{filename}.rb already exists")
+      else
+        migration_template "migrations/#{filename}.rb", "db/migrate/#{filename}.rb"
+      end
     end
   end
 end
